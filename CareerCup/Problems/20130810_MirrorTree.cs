ba496b2c2b84f38f35e77d117960190ea3cbaacf
ddewinter <david.dewinter@gmail.com>
2013-08-10 16:24:27 -0700

    Mirror BST: Handle null case.

--- CareerCup/Problems/20130810_MirrorTree.cs
+++ CareerCup/Problems/20130810_MirrorTree.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace MirrorTree
 {
-    public class MirrorTree
+    public class Problem
     {
-         
+        public Node MirrorTree(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            return root;
+        }
     }
 
     [TestFixture]
     public class MirrorTreeTests
     {
+        [Test]
+        public void Null_root_returns_null()
+        {
+            Node n = null;
+
+            var p = new Problem();
+            var result = p.MirrorTree(n);
+
+            result.Should().BeNull();
+        }
+    }
+
+    public class Node
+    {
+        public Node(string name)
+        {
+            this.Name = name;
+        }
+
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public string Name { get; private set; }
     }
 }
